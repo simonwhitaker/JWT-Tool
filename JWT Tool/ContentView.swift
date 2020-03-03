f70c5c850946510f486e373af89ba5a7dfb84d81
Simon Whitaker <sw@netcetera.org>
2020-03-03 05:59:20 +0000

    Initial Commit

---
+++ JWT Tool/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  JWT Tool
+//
+//  Created by Simon on 03/03/2020.
+//  Copyright © 2020 Simon Whitaker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
